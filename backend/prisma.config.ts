df1ee026e7562fdfc657c9b792c55b5966c389a7
PrashantKumar9786 <prashantkushwah913@gmail.com>
2026-06-08 00:06:43 +0530

    Remove Localhost database URL

--- backend/prisma.config.ts
+++ backend/prisma.config.ts
@@ -3,6 +3,6 @@ import { defineConfig } from "prisma/config";
 export default defineConfig({
   schema: "prisma/schema.prisma",
   datasource: {
-    url: "postgresql://postgres:hrms9785@localhost:5432/hrms",
+    url: process.env.DATABASE_URL,
   },
 });
